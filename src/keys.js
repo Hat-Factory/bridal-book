b3ac25afa186b5adbcc6d71bb87fac6dc3dc3e46
Alan Charles <33434787+abomb14c@users.noreply.github.com>
2018-10-11 13:24:42 -0600

    Delete keys.js

--- src/keys.js
+++
@@ -1,7 +0,0 @@
-export const accessKeyId = 'us-west-2:7fd17a79-c8e1-44ff-83b4-ed0957cc29fd';
-export const secretAccessKey = 'MthKWvdbjp3iWVWAmvRsKAc4bkSR8yy4uki+dP+b';
-
-export const aws = {
-  accessKeyId: 'AKIAIIFCGY6GAPZAPEBQ',
-  secretAccessKey: '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'
-}
